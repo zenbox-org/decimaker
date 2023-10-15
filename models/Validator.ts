319f7e1dfb62b978156134f77a33e7d3c403c6c7
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-10-15 17:40:58 +0700

    Thinking

--- models/Validator.ts
+++ models/Validator.ts
@@ -1,3 +1,5 @@
-import { Mapper } from '../../generic/models/Mapper'
+import { Mapper, MapperP } from '../../generic/models/Mapper'
 
 export type Validator<Val, Err> = Mapper<Val, Err[]>
+
+export type ValidatorP<Val, Err> = MapperP<Val, Err[]>

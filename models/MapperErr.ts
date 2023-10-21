6b70f4c5ff7b4595c6ce91a73be338b538468d05
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-10-21 16:24:20 +0700

    Mapper

--- models/MapperErr.ts
+++
@@ -1,5 +0,0 @@
-import { Mapper, MapperP } from '../../generic/models/Mapper'
-
-export type MapperErr<Val, Err> = Mapper<Val, Err[]>
-
-export type MapperErrP<Val, Err> = MapperP<Val, Err[]>
--- models/Verifier.ts
+++ models/Verifier.ts
@@ -1,7 +1,6 @@
 import { Filter } from '../../generic/models/Filter'
-import { MapperTwoSym } from '../../generic/models/Mapper'
+import { Mapper, MapperTwoSym } from '../../generic/models/Mapper'
 import { Asserter, AsserterTwoSym } from '../../utils/Asserter'
-import { MapperErr } from './MapperErr'
 
 /*
  * NOTE: Use original types
@@ -9,7 +8,7 @@ import { MapperErr } from './MapperErr'
  * The following code is just for demonstration purposes
  */
 
-export type VerifierReturnErrors<Val, Err> = MapperErr<Val, Err>
+export type VerifierReturnErrors<Val, Err> = Mapper<Val, Err[]>
 
 export type VerifierReturnBoolean<Val> = Filter<Val>
 
